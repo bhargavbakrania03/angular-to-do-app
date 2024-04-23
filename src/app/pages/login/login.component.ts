591a6cb3c8a2d4c3543814a5f3898accd1b821da
Bhargav Bakrania <bakraniabhargav@gmail.com>
2024-04-24 01:48:02 +0530

    initial commit

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // this.authService.onLogin(this.loginForm.value).subscribe({
+      //   next: () => {},
+      // });
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+}
